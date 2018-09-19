4eff5a8817dce84bcc712b06b41c99fe1b58b2cd
shouston3 <shouston3@me.com>
2018-09-19 18:06:36 +0100

    improve coverage

--- test/unit/specs/Dashboard.spec.js
+++ test/unit/specs/Dashboard.spec.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
@@ -7,5 +8,14 @@ describe('HelloWorld.vue', () => {
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App')
+  })
+})
+
+describe('Dashboard.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(Dashboard)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .to.equal('Dashboard')
   })
 })
